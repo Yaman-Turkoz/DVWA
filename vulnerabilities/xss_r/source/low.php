9a7e833e2add85b5780232676c5a14bdfdbcbdf9
Yaman Türköz <151014605+Yaman-Turkoz@users.noreply.github.com>
2026-04-30 11:13:08 +0300

    Rename variable from name21 to name213

--- vulnerabilities/xss_r/source/low.php
+++ vulnerabilities/xss_r/source/low.php
@@ -8,8 +8,8 @@ if( array_key_exists( "name", $_GET ) && $_GET[ 'name' ] != NULL ) {
 	$html .= '<pre>Hello ' . $_GET[ 'name' ] . '</pre>';
 }
 
-$name21 = $_GET["name"];
+$name213 = $_GET["name"];
 
-echo $name21;
+echo $name213;
 
 ?>
